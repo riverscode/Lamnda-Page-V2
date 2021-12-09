740ad7c8c08f5eb090a3c0faa7599a37ecc13714
riverscode <rios.renzo@pucp.pe>
2021-12-08 20:01:27 -0500

    Fix: La data en una carpeta individual / Add: Testimonios de Alumnos

---
+++ src/data/testimonial-data.js
@@ -0,0 +1,58 @@
+import Avatar1 from "assets/testimonial/avatar1.png";
+import Peru from "assets/country/country-peru.png";
+import Argentina from "assets/country/country-argentina.png";
+
+const testimonialData = [
+  {
+    id: 1,
+    title: "Buena propuesta para aprender herramientas tecnológicas.",
+    description:
+      "Muy recomendable para aprender cursos que son escasos o inasequibles en el mercado.",
+    avatar: Peru,
+    name: "Kevin Meza",
+    designation: "Perú",
+    review: 5,
+  },
+  {
+    id: 2,
+    title: "Inovador.",
+    description:
+      "El nivel inicial fue muy completo y organizado. Se nota que el docente sabe mucho y resolvia todas las dudas en clase.",
+    avatar: Argentina,
+    name: "Andres Singer",
+    designation: "Argentina",
+    review: 4,
+  },
+  {
+    id: 3,
+    title: "Eficaz en sus clases.",
+    description:
+      "Lograr objetivo de aprender, poner en práctica lo aprendido. Bueno y optimiza tiempos para entregables",
+    avatar: Peru,
+    name: "Carlos Alfredo Piscoya",
+    designation: "Perú",
+    review: 5,
+  },
+  {
+    id: 4,
+    title: "Recomendaría sus cursos de capacitación.",
+    description:
+      "A pesar de no tener ningún conocimiento en programación pude comprender todo el contenido del curso.",
+    avatar: Peru,
+    name: "Juan Gonzales",
+    designation: "Perú",
+    review: 4,
+  },
+  {
+    id: 5,
+    title: "Comprensible y práctico.",
+    description:
+      "El curso fue bastante bueno y ayudó a la comprensión del comportamiento de los elementos de Revit a un nivel más alto.",
+    avatar: Peru,
+    name: "Manuel Olortegui",
+    designation: "Perú",
+    review: 5,
+  },
+];
+
+export default testimonialData;
--- src/sections/course.js
+++ src/sections/course.js
@@ -3,61 +3,7 @@ import { jsx, Box, Container } from "theme-ui";
 import SectionHeader from "components/section-header";
 import PopularCard from "components/popular-card";
 
-const popularCourseData = [
-  {
-    title: "Automatización de procesos con Revit API - Básico",
-    description:
-      "Crea tus primeras aplicaciones con la API de Revit sin conocimiento en programación.",
-    reviewCount: "5.0",
-    watchCount: 85,
-    videoLink: "L9jU-IIN0ng",
-    starCount: 5,
-    projectsCount: 6,
-    hoursCount: 24,
-    expanded: true,
-    list: [
-      {
-        content: "Aprende los conceptoos basicos de la POO.",
-      },
-      {
-        content: "Comprende como funciona la API de Revit.",
-      },
-      {
-        content: "Automatiza la gestion de Informacion de Revit.",
-      },
-      {
-        content: "Crea tus primeras aplicaciones con la API de Revit.",
-      },
-    ],
-  },
-  {
-    title: "Automatización de procesos con Revit API - Intermedio/Avanzado",
-    description:
-      "Lleva tus conocimientos al siguiente nivel con proyectos reales.",
-    reviewCount: "4.5",
-    watchCount: 42,
-    videoLink: "L9jU-IIN0ng",
-    starCount: 4,
-    projectsCount: 14,
-    hoursCount: 36,
-    expanded: false,
-    list: [
-      {
-        content: "Comprende el proceso global del desarrollo de aplicaicones BIM.",
-      },
-      {
-        content: "Aplica tus conocimientos con proyectos reales.",
-      },
-      {
-        content: "Lleva al siguiente nivel el interfaz de tus aplicaciones.",
-      },
-      {
-        content: "Publica tus aplicaciones en Autodesk Store.",
-      },
-
-    ],
-  },
-];
+import popularCourseData from "data/course-data";
 
 export default function Course() {
   return (
