393683f6368a5ab00bf73117c8724e66c922d1a3
riverscode <rios.renzo@pucp.pe>
2021-12-11 23:49:20 -0500

    HotFix: Meta etiquetas Og

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,33 +1,11 @@
-import React from "react";
-import Head from "next/head";
+import React from 'react';
+import Head from 'next/head';
 
-{
-  /* <link rel="canonical" href="https://lambda.com.pe"></link> */
-}
-{
-  /* <meta name="theme-color" content="#13AA52" /> */
-}
-{
-  /* <meta name="description" content={description} /> */
-}
-
-{
-  /* <meta property="og:locale" content="es_ES" /> */
-}
-{
-  /* <meta property="og:title" content={title} /> */
-}
-{
-  /* <meta property="og:description" content={description} /> */
-}
-{
-  /* <meta property="og:type" content="website" /> */
-}
 export default function SEO({
-  description = "Aprende BIM con los mejores cursos online para la industria de la construción.",
-  author = "Lambda Ingeniería e Innovación",
+  description = 'Pagina principal de Lambda. La empresa mas innovadora del sector construccion.',
+  author = 'Lambda, Inc',
   meta,
-  title = "Lambda Ingenieria e innovacion",
+  title = 'Lambda Ingenieria e innovacion',
 }) {
   const metaData = [
     {
@@ -66,7 +44,6 @@ export default function SEO({
   return (
     <Head>
       <title>{title}</title>
-      {/* <meta property="og:image" content={previewImage} key="ogimage" /> */}
       {metaData.map(({ name, content }, i) => (
         <meta key={i} name={name} content={content} />
       ))}
@@ -75,6 +52,6 @@ export default function SEO({
 }
 
 SEO.defaultProps = {
-  lang: `en`,
+  lang: `es`,
   meta: [],
 };
