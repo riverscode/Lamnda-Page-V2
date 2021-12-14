127e8243bdddeca9704bdd77028ef90b3f742292
riverscode <rios.renzo@pucp.pe>
2021-12-13 21:34:44 -0500

    Test: Analitys

--- src/analytics/index.js
+++ src/analytics/index.js
@@ -2,7 +2,7 @@ import ReactGA from 'react-ga';
 
 export const initGA = () => {
   console.log('GA init');
-  ReactGA.initialize('UA-xxxxxxxxx-1');
+  ReactGA.initialize('G-9H70XCYC6K');
 };
 
 export const logPageView = () => {
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,39 @@
-import { useEffect } from 'react';
-import Router from 'next/router';
-import { initGA, logPageView } from 'analytics';
-import 'react-multi-carousel/lib/styles.css';
-import 'react-modal-video/css/modal-video.min.css';
-import 'rc-drawer/assets/index.css';
-import 'highlight.js/styles/xcode.css';
-import 'assets/css/styles.css'
-import 'typeface-dm-sans';
+import { useEffect } from "react";
+import Router from "next/router";
+import { initGA, logPageView } from "analytics";
+import "react-multi-carousel/lib/styles.css";
+import "react-modal-video/css/modal-video.min.css";
+import "rc-drawer/assets/index.css";
+import "highlight.js/styles/xcode.css";
+import "assets/css/styles.css";
+import "typeface-dm-sans";
+
 export default function CustomApp({ Component, pageProps }) {
   useEffect(() => {
     initGA();
     logPageView();
-    Router.events.on('routeChangeComplete', logPageView);
+    Router.events.on("routeChangeComplete", logPageView);
   }, []);
 
-  return <Component {...pageProps} />;
+
+  return (
+    <>
+    {/* <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+                `}
+      </Script> */}
+      <Component {...pageProps} />;
+    </>
+  );
 }
